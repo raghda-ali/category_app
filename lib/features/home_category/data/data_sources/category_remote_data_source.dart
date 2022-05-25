193c172f822945aad3d44cff7fc64ffe487f84cd
raghda-ali <raghdaali991@gmail.com>
2022-05-25 16:37:46 +0200

    remote data source test

--- lib/features/home_category/data/data_sources/category_remote_data_source.dart
+++ lib/features/home_category/data/data_sources/category_remote_data_source.dart
@@ -4,15 +4,15 @@ import 'package:dio/dio.dart';
 abstract class CategoryRemoteDataSource {
   Future<List<Category>> getHomeCategory();
 }
-
-class CategoryRemoteDataSourceImpl implements CategoryRemoteDataSource {
-  final Dio dio;
-
-  CategoryRemoteDataSourceImpl(this.dio);
-
-  @override
-  Future<List<Category>> getHomeCategory() {
-    // TODO: implement getHomeCategory
-    throw UnimplementedError();
-  }
-}
+//
+// class CategoryRemoteDataSourceImpl implements CategoryRemoteDataSource {
+//   final Dio dio;
+//
+//   CategoryRemoteDataSourceImpl(this.dio);
+//
+//   @override
+//   Future<List<Category>> getHomeCategory() {
+//     dio.get('https://ta3alapp.com/ta3al/Api/customer/get_category');
+//     return null;
+//   }
+// }
--- test/features/home_category/data/data_sources/category_remote_data_source_test.dart
+++ test/features/home_category/data/data_sources/category_remote_data_source_test.dart
@@ -1,18 +1,43 @@
+import 'dart:convert';
+
 import 'package:category_app_demo/features/home_category/data/data_sources/category_remote_data_source.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' as dio;
+
+import '../../../../fixtures/fixture.reader.dart';
 
 class MockDio extends Mock implements Dio {}
 
 void main() {
   late CategoryRemoteDataSourceImpl remoteDataSourceImpl;
-  late Dio dio;
+  late Dio mockDio;
   setUp(() {
-    dio = Dio();
-    remoteDataSourceImpl = CategoryRemoteDataSourceImpl(dio);
+    mockDio = Dio();
+    remoteDataSourceImpl = CategoryRemoteDataSourceImpl(mockDio);
   });
-  test('getCategory', () {
+  group('getHomeCategory', () {
+    test('should perform a GET request on a URL', () {
+      //arrange
+      when(() => mockDio.get(
+          'https://ta3alapp.com/ta3al/Api/customer/get_category')).thenAnswer(
+        (_) async => dio.Response(
+          data: jsonDecode(fixture('category.json')) as Map<String, dynamic>,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+      //act
+      remoteDataSourceImpl.getHomeCategory();
+      //assert
+      verify(() => mockDio.get(
+            'https://ta3alapp.com/ta3al/Api/customer/get_category',
+          ));
+    });
 
+    test('sh', () {
+
+    });
   });
 }
